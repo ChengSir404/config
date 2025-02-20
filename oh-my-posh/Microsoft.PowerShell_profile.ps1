2837eb89ba200b2e37dd72204c087b8c3391f676
cheng <chengwq0@gmail.com>
2025-02-20 16:05:52 +0800

    更新配置文件

--- oh-my-posh/Microsoft.PowerShell_profile.ps1
+++ oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,19 @@
-
-#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
-
-#Import-Module "D:\Application\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
-#34de4b3d-13a8-4540-b76d-b9e8d3851756
-
 $previousOutputEncoding = [Console]::OutputEncoding
 [Console]::OutputEncoding = [Text.Encoding]::UTF8
 try {
     #oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\sim-web.omp.json | Invoke-Expression
     #oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\markbull.omp.json | Invoke-Expression
-    oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\tokyonight_storm.omp.json | Invoke-Expression
+    #oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\tokyonight_storm.omp.json | Invoke-Expression
+    #oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\amro.omp.json | Invoke-Expression
+    oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\json.omp.json | Invoke-Expression
+    #oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\multiverse-neon.omp.json | Invoke-Expression
+    #oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\pure.omp.json | Invoke-Expression
+    #oh-my-posh init pwsh --config C:\Users\root\AppData\Local\Programs\oh-my-posh\themes\spaceship.omp.json | Invoke-Expression
 } finally {
     [Console]::OutputEncoding = $previousOutputEncoding
 	
 }
+
+
 
 
